059651fe4815c7e177a1e8e23d051040e17db573
Rifqi Fauzi <rifqi.thefi.fauzi@gmail.com>
2016-05-17 20:28:13 +0700

    initial landing page

--- Gemfile
+++ Gemfile
@@ -14,9 +14,6 @@ gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 gem 'therubyracer', platforms: :ruby
 
-# Use jquery as the JavaScript library
-gem 'jquery-rails'
-
 # A ruby web server built for concurrency
 gem 'puma'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,13 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+//= require application/jquery.min
+//= require application/bootstrap
+//= require application/flickr
+//= require application/flexslider.min
+//= require application/lightbox.min
+//= require application/masonry.min
+//= require application/twitterfetcher.min
+//= require application/spectragram.min
+//= require application/ytplayer.min
+//= require application/countdown.min
+//= require application/smooth-scroll.min
+//= require application/parallax
+//= require application/scripts
