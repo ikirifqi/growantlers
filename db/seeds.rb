021e183f8369408a09dfa0ef2c607865fbd860f9
Rifqi Fauzi <rifqi.thefi.fauzi@gmail.com>
2016-05-17 17:53:36 +0700

    install devise for admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Deleting tables content...'
+Admin.destroy_all
+
+puts 'Purging tables...'
+ActiveRecord::Base.connection.tables.each do |t|
+  if Rails.env.development?
+    update_seq_sql = "UPDATE sqlite_sequence SET seq = 0 WHERE name = '#{t}';"
+  else
+    update_seq_sql = "ALTER TABLE #{t} AUTO_INCREMENT = 1"
+  end
+  ActiveRecord::Base.connection.execute(update_seq_sql)
+end
