10c31ded235f28b4dd1b4f4023889dd8380fc8e5
Rifqi Fauzi <rifqi.thefi.fauzi@gmail.com>
2016-05-21 19:42:23 +0700

    setup pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
 
   root 'home#index'
+
+  get '/portfolio' => 'home#portfolio', :as => :portfolio
+  get '/contact' => 'home#contact', :as => :contact
 end
