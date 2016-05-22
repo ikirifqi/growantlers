55200893f4245f43d7392fc897c9d8309e4ac1d4
Rifqi Fauzi <rifqi.thefi.fauzi@gmail.com>
2016-05-22 10:19:51 +0700

    show portfolio page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,9 @@ class HomeController < ApplicationController
   def portfolio
   end
 
+  def show_portfolio
+  end
+
   def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get '/portfolio' => 'home#portfolio', :as => :portfolio
+  get '/portfolio/:slug_id' => 'home#show_portfolio', :as => :show_portfolio
   get '/contact' => 'home#contact', :as => :contact
 end
