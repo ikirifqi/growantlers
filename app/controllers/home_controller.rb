5fc88e16f65c24e06a65a6eecc8b10c0861c871d
Rifqi Fauzi <rifqi.thefi.fauzi@gmail.com>
2016-05-23 10:56:03 +0700

    make mailer works

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,18 +12,22 @@ class HomeController < ApplicationController
   end
 
   def submit_contact
-    if !params[:name].present? || !params[:email].present? || !params[:message].present?
-      redirect_to :back
-    else
+    result = 'Failed to submit your contact'
+
+    if params[:name].present? && params[:email].present? && params[:message].present?
       name = params[:name]
       email = params[:email]
       message = params[:message]
 
       # TODO: move to background process
-      AntlersMailer.send_notification_to_admin(name, email, message)
-      AntlersMailer.send_notification_to_visitor(name, email, message)
+      AntlersMailer.send_notification_to_admin(name, email, message).deliver_now
+      AntlersMailer.send_notification_to_visitor(name, email, message).deliver_now
 
-      redirect_to root_path
+      result = '1'
+    end
+
+    respond_to do |f|
+      f.js { render :json => result }
     end
   end
 end
