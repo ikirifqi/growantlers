6ce03fffc0ca09caa35433546abc33733bda787c
Rifqi Fauzi <rifqi.thefi.fauzi@gmail.com>
2016-06-13 12:59:59 +0700

    specify password

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "sudo /etc/init.d/unicorn_#{application} #{command}"
+      run "echo 'bismillah' | sudo -S /etc/init.d/unicorn_#{application} #{command}"
     end
   end
 
